8f5088d9b8464c14de4d91542729e5613d69231c
Sumanth bellamkonda <sumanthbellamkonda690@gmail.com>
2023-09-27 20:06:59 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,73 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Time Series Forecasting</title>
+</head>
+<body>
+    <h1>Time Series Forecasting</h1>
+    <label for="alpha">Alpha:</label>
+    <input type="text" id="alpha" placeholder="0.2"><br><br>
+    <label for="beta">Beta:</label>
+    <input type="text" id="beta" placeholder="0.2"><br><br>
+    <label for="gamma">Gamma:</label>
+    <input type="text" id="gamma" placeholder="0.2"><br><br>
+    <button onclick="forecast()">Forecast</button><br><br>
+    <div id="forecasted"></div>
 
+    <script>
+        function forecast() {
+            var alpha = document.getElementById('alpha').value;
+            var beta = document.getElementById('beta').value;
+            var gamma = document.getElementById('gamma').value;
 
-# TIME SERIES FORECASTING IN HOSPITAL
-#Step 1: Data Collection
-#Collect historical hospital data relevant to your forecasting task. This data can include patient admission records, resource utilization, disease outbreak records, and more. Ensure the data is well-structured and contains a timestamp.
-# Example: Load patient admission data from a CSV file
-import pandas as pd
-hospital_data = pd.read_csv('hospital_data.csv')
-#Step 2: Data Preprocessing
-#Clean and preprocess the data. Handle missing values, resample if needed (e.g., daily to monthly data), and ensure it's in a suitable format for time series analysis.
-# Example: Handle missing values and set the timestamp as the index
-hospital_data['Timestamp'] = pd.to_datetime(hospital_data['Timestamp'])
-hospital_data.set_index('Timestamp', inplace=True)
-hospital_data.fillna(0, inplace=True)
-#Step 3: Exploratory Data Analysis (EDA)
-#Conduct EDA to understand the data's characteristics, patterns, and seasonality. Visualize the data using plots and statistical analysis.
-# Example: Plot patient admission trends
-import matplotlib.pyplot as plt
-
-plt.figure(figsize=(12, 6))
-plt.plot(hospital_data.index, hospital_data['Admissions'], label='Patient Admissions')
-plt.title('Patient Admissions Over Time')
-plt.xlabel('Timestamp')
-plt.ylabel('Admissions')
-plt.legend()
-plt.show()
-#Step 4: Time Series Forecasting Model
-#Select an appropriate time series forecasting model. In this example, we'll use Facebook Prophet for simplicity.
-# Example: Time series forecasting using Prophet
-from fbprophet import Prophet
-
-model = Prophet()
-hospital_data.reset_index(inplace=True)
-hospital_data.rename(columns={'Timestamp': 'ds', 'Admissions': 'y'}, inplace=True)
-
-model.fit(hospital_data)
-#Step 5: Generate Forecasts
-#Generate forecasts for the desired time horizon.
-# Example: Generate future dates for forecasting
-future = model.make_future_dataframe(periods=365)  # Forecast for one year
-
-forecast = model.predict(future)
-#Step 6: Visualize Forecast Results
-#Plot the forecasted values along with confidence intervals.
-# Example: Plot forecast results
-fig = model.plot(forecast)
-plt.title('Hospital Patient Admissions Forecast')
-plt.xlabel('Date')
-plt.ylabel('Admissions')
-plt.show()
-#Step 7: Evaluation and Fine-tuning
-#Evaluate the forecasting model's performance using appropriate metrics (e.g., Mean Absolute Error, Mean Squared Error). Fine-tune the model if necessary.
-#Example: Evaluation using Mean Absolute Error
-from sklearn.metrics import mean_absolute_error
-
-actual_values = hospital_data['y']
-predicted_values = forecast['yhat'][:-365]  # Exclude the forecasted future values
-
-mae = mean_absolute_error(actual_values, predicted_values)
-print(f'Mean Absolute Error: {mae}')
-#Step 8: Deployment
-#-Integrate the forecasting model into your hospital's systems or applications to make real-time predictions if needed.
-#Step 9: Monitoring and Maintenance
-#-Regularly monitor the forecasting model's performance and update it with new data as it becomes available. Re-train the model as needed to keep it accurate.
-#-This project outline provides a structured approach to time series forecasting in a hospital setting. You can customize each step to fit your specific forecasting needs and data.
-
-
-
-
-
-
-
-
+            fetch('/forecast', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    alpha: parseFloat(alpha),
+                    beta: parseFloat(beta),
+                    gamma: parseFloat(gamma)
+                })
+            })
+            .then(response => response.json())
+            .then(result => {
+                document.getElementById('forecasted').textContent = "Forecasted Values: " + result.forecast.join(', ');
+            })
+            .catch(error => {
+                console.error(error);
+            });
+        }
+    </script>
+</body>
+</html>
